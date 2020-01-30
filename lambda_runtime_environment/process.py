cd1dfb5bbbc7437756b5bd64e131aa8cd2ae17e3
EC2 Default User <ec2-user@ip-172-31-82-170.ec2.internal>
2020-01-30 18:11:30 +0000

    add runtime scripts and notes on how to setup a runtime environment for lambda

---
+++ lambda_runtime_environment/process.py
@@ -0,0 +1,5 @@
+import subprocess
+
+proc = subprocess.Popen(['/var/task/hstcal/bin/calacs.e', '--version]'], shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+(stdout, stderr) = proc.communicate()
+print(f'stdout: {stdout}')
