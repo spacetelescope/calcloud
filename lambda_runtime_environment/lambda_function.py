f45f8690d1a3ca85361575d14655125a3e5d4580
EC2 Default User <ec2-user@ip-172-31-82-170.ec2.internal>
2020-01-30 18:13:47 +0000

    reformat py files

--- lambda_runtime_environment/lambda_function.py
+++ lambda_runtime_environment/lambda_function.py
@@ -6,8 +6,10 @@ import time
 def lambda_handler(event, context):
     print(f'Received event: {json.dumps(event, indent = 4)}')
 
-    print('Calling process.py')
-    proc = subprocess.Popen(['python process.py'], shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    proc = subprocess.Popen(
+        ['python process.py'], shell = True,
+        stdout = subprocess.PIPE, stderr = subprocess.PIPE
+    )
     while proc.poll() is None:
         time.sleep(0.1)
         print(proc.stdout.read())
--- lambda_runtime_environment/process.py
+++ lambda_runtime_environment/process.py
@@ -1,5 +1,8 @@
 import subprocess
 
-proc = subprocess.Popen(['/var/task/hstcal/bin/calacs.e', '--version]'], shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+proc = subprocess.Popen(
+    ['/var/task/hstcal/bin/calacs.e', '--version]'], shell = True,
+    stdout = subprocess.PIPE, stderr = subprocess.PIPE
+)
 (stdout, stderr) = proc.communicate()
 print(f'stdout: {stdout}')
