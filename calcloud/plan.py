710f267a93c6744c59decf68f19164895422b1bd
EC2 Default User <ec2-user@ip-10-142-20-14.ec2.internal>
2021-02-18 20:42:03 +0000

    Untested baseline of "control" messaging in submit lambda needed for searching for Batch jobs by ID and/or tracking memory-driven retries.
    Refactored top level functions in calcloud.plan and calcloud.provision
    to expose simpler ipppssoot-by-ipppssoot functions better tuned to the
    submit lambda; planning and provisioning compute will change but those
    APIs may last. The CLI still works for potential model integration.
    Re-categorized some functions as private so they'll be easier to
    change/delete during refactoring.

--- calcloud/plan.py
+++ calcloud/plan.py
@@ -5,8 +5,6 @@ The idea behind creating resourcess is to generate enough information such that 
 set of ipppssoots can be assigned to well tuned processing resources.
 """
 import sys
-import datetime
-import math
 import os
 from collections import namedtuple
 
@@ -15,15 +13,6 @@ from . import metrics
 from . import log
 
 # ----------------------------------------------------------------------
-
-# def get_batch_name(name):
-#     when = datetime.datetime.now().isoformat()
-#     when = when.replace(":","-")
-#     when = when.replace(".","-")
-#     return name + "-" + when[:-7]   # drop subseconds
-
-# One key function of JOB_DEFINITION appears to be indelible declaration of Docker image.
-# New image == new definition required.
 
 """
 WFC3: 95 per duration 0.93 hr
@@ -42,6 +31,8 @@ STIS: 95 per duration 0.12 hr
 STIS: 99 per duration 0.24 hr
 STIS: 95 per mem 0.64 gb
 STIS: 99 per mem 1.0 gb
+
+
 """
 
 # For optimization with AWS Batch, key insights are:
@@ -65,9 +56,20 @@ STIS: 99 per mem 1.0 gb
 #                 An integer multiple isn't required.
 #
 # NOTE:  for the initial prototype,  the job definition is named calcloud-hst-caldp-job-definition
-JobResources = namedtuple("JobResources", [
-    "ipppssoot", "instrument", "job_name", "s3_output_uri", "input_path", "crds_config", "vcpus", "memory", "max_seconds"
-    ])
+JobResources = namedtuple(
+    "JobResources",
+    [
+        "ipppssoot",
+        "instrument",
+        "job_name",
+        "s3_output_uri",
+        "input_path",
+        "crds_config",
+        "vcpus",
+        "memory",
+        "max_seconds",
+    ],
+)
 
 # Conceptually the resource requirements defined here could be obtained from a database that records real-world
 # memory, cpu, and expected runtime on a per-IPPPSSOOT basis.   The exact values in the tuples are less important
@@ -75,36 +77,46 @@ JobResources = namedtuple("JobResources", [
 # the knobs which can be used to do optimization,  some more complex than others:  e.g. a queue and it's associated
 # compute environment is complex,  but memory or CPU consumption are simple values.
 
-JOB_INFO = { # cores, memory M, time secs
-    "acs" : (4, int(4*1024),  int(60*60*6.5)),
-    "cos" : (1, int(1.25*1024), int(60*20)),
-    "stis" : (1, int(1.25*1024), int(60*20)),
-    "wfc3" : (4, int(4*1024), int(60*60*2)),
-    }
+# JOB_INFO = { # cores, memory M, time secs
+#     "acs" : (4, int(4*1024),  int(60*60*6.5)),
+#     "cos" : (1, int(1.25*1024), int(60*20)),
+#     "stis" : (1, int(1.25*1024), int(60*20)),
+#     "wfc3" : (4, int(4*1024), int(60*60*2)),
+#     }
 
-def get_job_resources(instr, ipppssoot):
+
+def _get_job_resources(instr, ipppssoot, retries=0):
     """Given the instrument `instr` and dataset id `ipppssoot`...
 
     Return  required resources (cores, memory in M,  seconds til kill)
 
     Note that these are "required" and still need to be matched to "available".
     """
-    info = list(JOB_INFO[instr.lower()])
+    info = [0] * 3
     try:
         memory_megabytes, cpus, wallclock_seconds = metrics.get_resources(ipppssoot)
         info[0] = cpus
-        info[1] = memory_megabytes + 512    # add some overhead for AWS Batch (>= 32M) and measurement error
-        info[2] = max(int(wallclock_seconds*cpus*6),120)  # kill time,  so too high is better than too low
+        info[1] = memory_megabytes + 512  # add some overhead for AWS Batch (>= 32M) and measurement error
+        info[2] = max(int(wallclock_seconds * cpus * 6), 120)  # kill time,  BEWARE:  THIS LOOKS WRONG
     except KeyError:
-        info = (32, int(64*1024), int(60*60*48))    # 32 cores,  64G/72G,  48 hours max   (c5.9xlarge)
+        info = (32, int(64 * 1024), int(60 * 60 * 48))  # 32 cores,  64G/72G,  48 hours max   (c5.9xlarge)
         log.warning("Defaulting (cpu, memory, time) requirements for unknown dataset:", ipppssoot, "to", info)
     return tuple(info)
 
-def get_resources(ipppssoot, output_bucket, input_path):
+
+def get_resources(ipppssoot, output_bucket, input_path, retries=0):
     """Given an HST IPPPSSOOT ID,  return information used to schedule it as a batch job.
 
-    Conceptually resource requirements can be tailored to individual IPPPSSOOTs driven
-    by a database lookup.
+    Conceptually resource requirements can be tailored to individual IPPPSSOOTs.
+
+    The job tuples define key parameters such as:
+
+    - ipppssoot to process              'o8jhg2nnq'
+    - Where to put outputs:             's3://calcloud-processing/outputs/o8jhg2nnq'
+    - Where to find inputs:             's3://calcloud-processing/inputs'
+    - Required vcpus:                   1
+    - Required memory in megabytes      512
+    - CPU seconds til kill              300
 
     Returns:  JobResources named tuple
     """
@@ -114,46 +126,40 @@ def get_resources(ipppssoot, output_bucket, input_path):
     job_name = ipppssoot
     input_path = input_path
     crds_config = "caldp-config-offsite"
-    return JobResources(*(ipppssoot, instr, job_name, s3_output_uri, input_path, crds_config) + get_job_resources(instr, ipppssoot))
+    return JobResources(
+        *(ipppssoot, instr, job_name, s3_output_uri, input_path, crds_config)
+        + _get_job_resources(instr, ipppssoot, retries)
+    )
 
-def get_resources_tuples(ipppssoots, output_bucket=f"s3://{os.environ['S3_PROCESSING_BUCKET']}", input_path=f"s3://{os.environ['S3_PROCESSING_BUCKET']}"):
-    """
-    Given an S3 `output_bucket` name string,
-    and a list of IPPPSSOOT dataset IDs `ipppssoots`, get_resources_tuples() return a list of JobResources()
-    tuples such that each IPPPSSOOT is handled once.
 
-    The job tuples define key parameters such as:
-
-    - ipppssoot to process
-    - Where to put outputs:  's3://calcloud-hst-pipeline-outputs/batch-10-2020-01-31T14-48-20/stis/O8JHG2NNQ'
-    - Required cores:  1
-    - Required memory: 512
-    - CPU seconds til kill: 300
-
-    >> from pprint import pprint
-    >> pprint(get_resources_tuples(["O8JHG2NNQ", "O8T9JEHXQ", "O4QPKTDCQ", "O6DCAQK9Q"]))  # doctest: +ELLIPSIS
-
-    """
-    return [get_resources(ipppssoot, output_bucket, input_path) for ipppssoot in ipppssoots
-            if hst.IPPPSSOOT_RE.match(ipppssoot.upper())]
-
-def planner(ipppssoots_file,  output_bucket=f"s3://{os.environ['S3_PROCESSING_BUCKET']}", input_path=f"s3://{os.environ['S3_PROCESSING_BUCKET']}"):
+def _planner(
+    ipppssoots_file,
+    output_bucket=f"s3://{os.environ['S3_PROCESSING_BUCKET']}",
+    input_path=f"s3://{os.environ['S3_PROCESSING_BUCKET']}",
+):
     """Given a set of ipppssoots in `ipppssoots_file` separated by spaces or newlines,
     as well as an `output_bucket` to define how the jobs are named and
     where outputs should be stored,  print out the associated batch resources tuples which
     can be submitted.
     """
-    with open(ipppssoots_file) as f:
-        ipppssoots = [ipppssoot.lower() for ipppssoot in f.read().split()]
-    for p in get_resources_tuples(ipppssoots, output_bucket, input_path):
-        print(tuple(p))  # Drop type to support literal_eval() vs. eval()
+    for line in open(ipppssoots_file).readlines():
+        if line.strip().startswith("#"):
+            continue
+        for ipst in line.split():
+            print(
+                tuple(get_resources(ipst, output_bucket, input_path))
+            )  # Drop type to support literal_eval() vs. eval()
+
 
 # ----------------------------------------------------------------------
+
 
 def test():
     import doctest
     from calcloud import plan
+
     return doctest.testmod(plan, optionflags=doctest.ELLIPSIS)
+
 
 if __name__ == "__main__":
     if len(sys.argv) in [2, 3, 4]:
@@ -163,6 +169,6 @@ if __name__ == "__main__":
             # ipppssoots_file = sys.argv[1]
             # output_bucket = sys.argv[2]  # 's3://calcloud-hst-pipeline-outputs'
             # batch_name = sys.argv[3]  #  'calcloud-hst-test-batch'
-            planner(*sys.argv[1:])
+            _planner(*sys.argv[1:])
     else:
         print("usage: python -m calcloud.plan  <ipppssoots_file>  [<output_bucket>]  [<batch_name>]", file=sys.stderr)
--- calcloud/provision.py
+++ calcloud/provision.py
@@ -14,43 +14,67 @@ from collections import namedtuple
 from . import plan
 from .plan import JobResources
 
-OUTLIER_THRESHHOLD_MEGABYTES = 8192-128  # m5.large - 128M ~overhead
+# -------------------------------------------------------------------------------
 
-JobEnv = namedtuple(
-    "JobEnv", ("job_queue", "job_definition", "command"))
+# XXXX during refactor,  hopefully keep interfaces:
+#
+#    Plan                     --  named tuple used to describe the result of get_plan()
+#
+#    get_plan(job_resources)  -- convert requirements defined by job_resources to a Plan
+#
+# but re-define what get_plan() actually does in terms of modelling
+# and predefined Batch resources.
+#
+
+OUTLIER_THRESHHOLD_MEGABYTES = 8192 - 128  # m5.large - 128M ~overhead
+
+JobEnv = namedtuple("JobEnv", ("job_queue", "job_definition", "command"))
 
 Plan = namedtuple("Plan", JobResources._fields + JobEnv._fields)
 
-def main(resource_file):
-    handle = sys.stdin if resource_file == "-" else open(resource_file,"r")
-    resources = []
-    for line in handle.read().splitlines():
-        resource_tup = ast.literal_eval(line)
-        resources.append(resource_tup)
-    for p in get_plan_tuples(resources):
-        print(tuple(p))
-
-def get_plan_tuples(job_resources_list):
-    return [ get_plan(plan.JobResources(*resource_reqs))
-             for resource_reqs in job_resources_list ]
 
 def get_plan(job_resources):
-    env = get_environment(job_resources)
+    """Given the resource requirements for a job,  map them onto appropriate
+    Batch resources needed to process the job.
+
+    Returns a Plan named tuple
+    """
+    env = _get_environment(job_resources)
     return Plan(*(job_resources + env))
 
-def get_environment(job_resources):
+
+# -------------------------------------------------------------------------------
+
+
+def _get_environment(job_resources):
     job_resources = JobResources(*job_resources)
-    job_definition = os.environ['JOBDEFINITION']
-    normal_queue = os.environ['NORMALQUEUE']
-    outlier_queue = os.environ['OUTLIERQUEUE']
+    job_definition = os.environ["JOBDEFINITION"]
+    normal_queue = os.environ["NORMALQUEUE"]
+    outlier_queue = os.environ["OUTLIERQUEUE"]
 
     if job_resources.memory <= OUTLIER_THRESHHOLD_MEGABYTES:
         return JobEnv(normal_queue, job_definition, "caldp-process")
     else:
         return JobEnv(outlier_queue, job_definition, "caldp-process")
 
+
+# -------------------------------------------------------------------------------
+
+
+def _main(resource_file):
+    """Run this on the output of plan.py to generate complete Plan tuples
+    which can be inspected or piped into submit.py.
+    """
+    handle = sys.stdin if resource_file == "-" else open(resource_file, "r")
+
+    for line in handle.read().splitlines():
+        resource_tup = ast.literal_eval(line)
+        plan = get_plan(resource_tup)
+        print(tuple(plan))
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("usage: provision <resource_reqs_file> | -")
         sys.exit(1)
-    main(sys.argv[1])
+    _main(sys.argv[1])
