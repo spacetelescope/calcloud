27a8d8d529ee8a3bbe3a773b5df2dd04bb86cb38
EC2 Default User <ec2-user@ip-10-142-20-14.ec2.internal>
2021-02-18 20:42:03 +0000

    Untested baseline of "control" messaging in submit lambda needed for searching for Batch jobs by ID and/or tracking memory-driven retries.
    Refactored top level functions in calcloud.plan and calcloud.provision
    to expose simpler ipppssoot-by-ipppssoot functions better tuned to the
    submit lambda; planning and provisioning compute will change but those
    APIs may last. The CLI still works for potential model integration.
    Re-categorized some functions as private so they'll be easier to
    change/delete during refactoring.

--- calcloud/lambda_submit.py
+++ calcloud/lambda_submit.py
@@ -4,36 +4,41 @@ This is intended for use in an AWS Lambda function where there is no user potent
 from . import plan
 from . import provision
 from . import submit
-from . import common
-import os
-import boto3
 
+from . import io
+
+import os
 
 # bucket name will need to be env variable?
 def main(ipppssoots_file, bucket_name=os.environ["S3_PROCESSING_BUCKET"]):
-    # reproduces plan.planner tuples that would normally be dumped to file
-    with open(ipppssoots_file) as f:
-        ipppssoots = [ipppssoot.lower() for ipppssoot in f.read().split()]
+
+    messages = io.get_message_api(bucket_name)
+    control = io.get_message_api(bucket_name)
 
     bucket = f"s3://{bucket_name}"
     input_path = f"{bucket}/inputs"
-    planned_resource_tuples = plan.get_resources_tuples(ipppssoots, bucket, input_path)
 
-    # reproduces the printed output of provision
-    provisioned_resource_tuples = provision.get_plan_tuples(planned_resource_tuples)
+    for ipppssoot in open(ipppssoots_file).read().split():
 
-    s3_client = boto3.resource("s3", config=common.retry_config)
+        ipppssoot = ipppssoot.lower()
 
-    # submits the jobs
-    for p in provisioned_resource_tuples:
         try:
-            submit.submit_job(p)
+            ctrl_msg = control.get(ipppssoot)
+            ctrl_msg["retries"] += 1
+        except control.client.exceptions.NoSuchKey:
+            ctrl_msg = dict(retries=0)
+
+        resources = plan.get_resources(ipppssoot, bucket, input_path, ctrl_msg["retries"])
+
+        provisioned = provision.get_plan_tuple(resources)
+
+        try:
+            response = submit.submit_job(provisioned)
+            ctrl_msg["job_id"] = response["jobId"]
         except Exception as e:
             print(e)
             continue
 
-        # pass the message
-        old_file_key = f"messages/placed-{p.ipppssoot}"
-        new_file_key = f"messages/submit-{p.ipppssoot}"
-        s3_client.Object(bucket_name, new_file_key).copy_from(CopySource=f"{bucket_name}/{old_file_key}")
-        s3_client.Object(bucket_name, old_file_key).delete()
+        control.put(ctrl_msg)
+
+        messages.move("placed-" + ipppssoot, "submit-" + ipppssoot)
