3b94c52f2b02b9b03a2eb48d591c8d84d4d306e2
EC2 Default User <ec2-user@ip-10-142-20-14.ec2.internal>
2021-02-20 15:52:30 +0000

    Refactored rescue_handler and  s3_trigger_hander to enable s3_trigger_handler to clear out existing control messages,  both now call lambda_submit.main directly. Refactored calcloud/lambda_submit.main to use simpler single ipppssoot API vs. ipst file. Overhauled code to use get_io_bundle()

--- lambda/s3_trigger/s3_trigger_handler.py
+++ lambda/s3_trigger/s3_trigger_handler.py
@@ -1,21 +1,24 @@
+from calcloud import lambda_submit
+from calcloud import io
+
+
 def lambda_handler(event, context):
-    import os
-    from calcloud import lambda_submit
+
     print(event)
 
     # events should only enter this lambda if their prefix is correct in s3
     # so we don't need any message validation here. In theory
-    message = event['Records'][0]['s3']['object']['key']
-    ipst = message.split('-')[-1]
-    print(message)
 
-    ipst_file = f'/tmp/{ipst}.txt'
-    with open(ipst_file, 'w') as f:
-        f.write(f"{ipst}\n")
+    bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+    message = event["Records"][0]["s3"]["object"]["key"]
+    ipst = message.split("-")[-1]
+    print(bucket_name, message, ipst)
 
-    lambda_submit.main(ipst_file)
-    # print(f"submit {ipst_file} here")
+    comm = io.get_io_bundle(bucket_name)
+    comm.control.delete("all-" + ipst)
+    comm.messages.delete("all-" + ipst)
+    comm.outputs.delete("all-" + ipst)
 
-    os.remove(ipst_file)
+    lambda_submit.main(ipst)
 
     return None
