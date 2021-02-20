85d5c4d4eb84db641759f56147760d050cbb3338
EC2 Default User <ec2-user@ip-10-142-20-14.ec2.internal>
2021-02-20 17:51:07 +0000

    Fix for batch event continuation message

--- lambda/batch_events/batch_event_handler.py
+++ lambda/batch_events/batch_event_handler.py
@@ -30,19 +30,19 @@ def lambda_handler(event, context):
     #  XXXXX Automatic rescue with increasing memory retry count
     if "memory_retries" not in ctrl_msg:
         ctrl_msg["memory_retries"] = 0
-    
-    continuation_type = "error-"
+
+    continuation_msg = "error-" + ipppssoot
     if fail_reason.startswith("OutOfMemoryError:"):
         if ctrl_msg["memory_retries"] < os.environ["MAX_MEMORY_RETRIES"]:
             print("Automatic rescue of", ipppssoot, "with memory retry count", ctrl_msg["memory_retries"])
             ctrl_msg["memory_retries"] += 1
-            continuation_type = "rescue-"
+            continuation_msg = "rescue-" + ipppssoot
         else:
             print("Automatic memory retries for", ipppssoot, "exhausted at", ctrl_msg["memory_retries"])
     else:
         print("Failure for", ipppssoot, "no automatic retry for", fail_reason)
-        
+
     # XXXX Since retry count used in planning, control output must precede rescue message
     comm.control.put(ipppssoot, ctrl_msg)
-    comm.messages.put("error-" + ipppssoot)
+    comm.messages.put(continuation_msg)
     print(ctrl_msg)
