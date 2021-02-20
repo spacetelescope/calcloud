e7c5c66e5843b4079ae6f082804bbb3fc097a7a6
EC2 Default User <ec2-user@ip-10-142-20-14.ec2.internal>
2021-02-20 15:45:54 +0000

    Overhauled s3.py and io.py to trim down public apis and boilerplate.

--- calcloud/io.py
+++ calcloud/io.py
@@ -11,23 +11,19 @@ from calcloud import s3
 # -------------------------------------------------------------
 
 __all__ = [
-    "get_message_api",
-    "get_output_api",
-    "get_input_api",
-    "S3Api",
-    "MessageApi",
-    "InputApi",
-    "OutputApi",
-    "MSG",
-    "IN",
-    "OUT",
+    "get_io_bundle",
     "MESSAGE_TYPES",
+    "S3Io",
+    "MessageIo",
+    "ControlIo",
+    "InputsIo",
+    "OutputsIo",
 ]
 
 # -------------------------------------------------------------
 
 
-class S3Api:
+class S3Io:
     """This class provides and API for S3 which implements basic
     put/get/list/del operations,  particularly as they pertain to
     messaging.
@@ -43,7 +39,7 @@ class S3Api:
         used which is presumed only suitable for single threaded
         or single process applications.
         """
-        self.client = client or s3.DEFAULT_S3_CLIENT
+        self.client = client or s3.get_default_client()
         self.s3_path = s3_path
 
     def path(self, prefix):
@@ -71,7 +67,7 @@ class S3Api:
         both the bucket and beginning of the S3 object key.
 
         This method is most important when overriden by subclasses,  particularly
-        the MessageApi subclass which translates "all-" into a sequence of all
+        the MessageIo subclass which translates "all-" into a sequence of all
         message types.
         """
         if isinstance(prefixes, str):
@@ -160,8 +156,8 @@ MESSAGE_TYPES = [
 ]
 
 
-class MessageApi(S3Api):
-    """The MessageApi class provides put/get/list/delete operations for
+class MessageIo(S3Io):
+    """The MessageIo class provides put/get/list/delete operations for
     messages of various types used to track the state of dataset
     processing.
 
@@ -177,41 +173,43 @@ class MessageApi(S3Api):
     It can operate on all types of messages for all ipppssoots,
     e.g. 'all'.
 
-    >>> MSG.put(['cancel-lcw303cjq', 'error-lcw303cjq', 'placed-lcw303cjq', 'rescue-lcw303cjq']);
-    >>> MSG.listl()
+    >>> comm = get_io_bundle()
+
+    >>> comm.messages.put(['cancel-lcw303cjq', 'error-lcw303cjq', 'placed-lcw303cjq', 'rescue-lcw303cjq']);
+    >>> comm.messages.listl()
     ['cancel-lcw303cjq', 'error-lcw303cjq', 'placed-lcw303cjq', 'rescue-lcw303cjq']
 
-    >>> list(MSG.list_s3("error")) #doctest: +ELLIPSIS
+    >>> list(comm.messages.list_s3("error")) #doctest: +ELLIPSIS
     ['s3://.../messages/error-lcw303cjq']
 
-    >>> MSG.listl("rescue")
+    >>> comm.messages.listl("rescue")
     ['rescue-lcw303cjq']
 
-    >>> MSG.delete("rescue");
-    >>> MSG.listl()
+    >>> comm.messages.delete("rescue");
+    >>> comm.messages.listl()
     ['cancel-lcw303cjq', 'error-lcw303cjq', 'placed-lcw303cjq']
 
-    >>> MSG.delete("all");
-    >>> MSG.listl()
+    >>> comm.messages.delete("all");
+    >>> comm.messages.listl()
     []
 
-    >>> list(MSG.expand_all('all-lcw303cjq'))
+    >>> list(comm.messages.expand_all('all-lcw303cjq'))
     ['placed-lcw303cjq', 'submit-lcw303cjq', 'processing-lcw303cjq', 'processed-lcw303cjq', 'error-lcw303cjq', 'ingesterror-lcw303cjq', 'ingested-lcw303cjq', 'terminated-lcw303cjq', 'cancel-lcw303cjq', 'rescue-lcw303cjq']
 
-    >>> MSG.put(['cancel-lcw303cjq', 'error-lcw303cjq', 'rescue-lcw304cjq']);
-    >>> MSG.delete('all-lcw303cjq');
-    >>> MSG.listl()
+    >>> comm.messages.put(['cancel-lcw303cjq', 'error-lcw303cjq', 'rescue-lcw304cjq']);
+    >>> comm.messages.delete('all-lcw303cjq');
+    >>> comm.messages.listl()
     ['rescue-lcw304cjq']
 
-    >>> MSG.put(['error-lcw303cjq', 'error-lcw304cjq', 'error-lcw305cjq']);  MSG.listl()
+    >>> comm.messages.put(['error-lcw303cjq', 'error-lcw304cjq', 'error-lcw305cjq']);  comm.messages.listl()
     ['error-lcw303cjq', 'error-lcw304cjq', 'error-lcw305cjq', 'rescue-lcw304cjq']
-    >>> MSG.delete('error-all');  MSG.listl()
+    >>> comm.messages.delete('error-all');  comm.messages.listl()
     ['rescue-lcw304cjq']
 
-    >>> MSG.move('rescue-lcw304cjq', 'terminated-lcw304cjq'); MSG.listl();
+    >>> comm.messages.move('rescue-lcw304cjq', 'terminated-lcw304cjq'); comm.messages.listl();
     ['terminated-lcw304cjq']
 
-    >>> MSG.delete("all")
+    >>> comm.messages.delete("all")
     """
 
     add_trigger_types = ["processed"]
@@ -220,18 +218,19 @@ class MessageApi(S3Api):
         """Message `prefix` should always start with at least the `type` aspect of
         a type-ipppssoot message id,  or 'all' types.
 
-        >>> MSG.path('rescue-lcw304cjq')  #doctest: +ELLIPSIS
+        >>> comm = get_io_bundle()
+        >>> comm.messages.path('rescue-lcw304cjq')  #doctest: +ELLIPSIS
         's3://.../messages/rescue-lcw304cjq'
         """
         parts = prefix.split("-")
         if parts[0] not in MESSAGE_TYPES + ["all"]:
             raise ValueError("Invalid message type for prefix: " + repr(prefix))
-        if parts[0] in self.add_trigger_types and len(parts)==2:   #  XXXXX whoop whoop whoop,  .trigger hack!!
-            prefix += ".trigger"
+        # if parts[0] in self.add_trigger_types and len(parts) == 2:  # XXXXX whoop whoop whoop,  .trigger hack!!
+        #     prefix += ".trigger"
         return self.s3_path + "/" + prefix
 
     def expand_prefix(self, prefix):
-        """For the MessageApi,  expand_all is used to generate a sequence of partial S3 keys
+        """For the MessageIo,  expand_all is used to generate a sequence of partial S3 keys
         which correspond to:
 
         1. "all messages of all types"    when prefix="all"
@@ -239,16 +238,17 @@ class MessageApi(S3Api):
         3. "one type for one ipppssoot"   when prefix="{type}-{ipst}"
         4. "all ipppssoots of one type"   when prefix="{type}-{all}" also equivalent to "{type}"
 
-        >>> list(MSG.expand_all('all'))
+        >>> comm = get_io_bundle()
+        >>> list(comm.messages.expand_all('all'))
         ['placed', 'submit', 'processing', 'processed', 'error', 'ingesterror', 'ingested', 'terminated', 'cancel', 'rescue']
 
-        >>> list(MSG.expand_all('all-lcw303cjq'))
+        >>> list(comm.messages.expand_all('all-lcw303cjq'))
         ['placed-lcw303cjq', 'submit-lcw303cjq', 'processing-lcw303cjq', 'processed-lcw303cjq', 'error-lcw303cjq', 'ingesterror-lcw303cjq', 'ingested-lcw303cjq', 'terminated-lcw303cjq', 'cancel-lcw303cjq', 'rescue-lcw303cjq']
 
-        >>> list(MSG.expand_all('rescue-lcw304cjq'))
+        >>> list(comm.messages.expand_all('rescue-lcw304cjq'))
         ['rescue-lcw304cjq']
 
-        >>> list(MSG.expand_all('rescue-all'))
+        >>> list(comm.messages.expand_all('rescue-all'))
         ['rescue']
         """
         if prefix == "all":
@@ -263,48 +263,52 @@ class MessageApi(S3Api):
             yield prefix
 
 
-class InputApi(S3Api):
-    """The InputApi provides simple standard operations on the processing
+class InputsIo(S3Io):
+    """InputsIo provides simple standard operations on the processing
     inputs store.
 
-    >>> IN.path('lcw303cjq') #doctest: +ELLIPSIS
+    >>> comm = get_io_bundle()
+    >>> comm.inputs.path('lcw303cjq') #doctest: +ELLIPSIS
     's3://.../inputs/lcw303cjq'
     """
 
 
-class OutputApi(S3Api):
-    """The OutputApi provides simple standard operations on the processing
+class OutputsIo(S3Io):
+    """OutputsIo provides simple standard operations on the processing
     outputs store.
 
-    >>> OUT.put({'lcw303cjq/something.fits': 'some contents'})
-    >>> OUT.get('lcw303cjq/something.fits')
+    >>> comm = get_io_bundle()
+    >>> comm.outputs.put({'lcw303cjq/something.fits': 'some contents'})
+    >>> comm.outputs.get('lcw303cjq/something.fits')
     'some contents'
 
-    >>> OUT.put({'lcw303cjq/something.fits': b'some contents'}, encoding=None)
-    >>> OUT.get('lcw303cjq/something.fits', encoding=None)
+    >>> comm.outputs.put({'lcw303cjq/something.fits': b'some contents'}, encoding=None)
+    >>> comm.outputs.get('lcw303cjq/something.fits', encoding=None)
     b'some contents'
 
-    >>> OUT.put('lcw303cjq/something.fits')
-    >>> OUT.get('lcw303cjq/something.fits')
+    >>> comm.outputs.put('lcw303cjq/something.fits')
+    >>> comm.outputs.get('lcw303cjq/something.fits')
     ''
 
-    >>> list(OUT.list_s3("all"))   #doctest: +ELLIPSIS
+    >>> list(comm.outputs.list_s3("all"))   #doctest: +ELLIPSIS
     ['s3://.../outputs/lcw303cjq/something.fits']
 
-    >>> OUT.delete("all");   list(OUT.list_s3())
+    >>> comm.outputs.delete("all");   list(comm.outputs.list_s3())
     []
     """
 
 
-class ControlApi(S3Api):
+class ControlIo(S3Io):
     """Provides simple standard operations on the processing control store,
     transparently serializing/de-serializing Python objects as a JSON
     encoded payload.
 
-    >>> obj = dict(job_params = dict(memory=1500, vcpus=2), job_id='1cc5817c-8d93-4119-bbd4-25407ee233b5', retry=0)
-    >>> CTRL.put('lcw303cjq', obj)
+    >>> comm = get_io_bundle()
 
-    >>> CTRL.get('lcw303cjq')
+    >>> obj = {'job_params': {'memory': 1500, 'vcpus': 2}, 'job_id': '1cc5817c-8d93-4119-bbd4-25407ee233b5', 'retry': 0}
+    >>> comm.control.put('lcw303cjq', obj)
+
+    >>> comm.control.get('lcw303cjq')
     {'job_params': {'memory': 1500, 'vcpus': 2}, 'job_id': '1cc5817c-8d93-4119-bbd4-25407ee233b5', 'retry': 0}
     """
 
@@ -321,38 +325,21 @@ class ControlApi(S3Api):
 # -------------------------------------------------------------
 
 
-def get_message_api(bucket=s3.DEFAULT_S3_BUCKET, client=None):
-    """Return an API for working with messages using S3 `client`
-    relative to S3 `bucket`.
-    """
-    return MessageApi("s3://" + bucket + "/messages", client)
+class IoBundle:
+    """Bundle all the I/O branches into one package."""
+
+    def __init__(self, bucket=s3.DEFAULT_S3_BUCKET, client=None):
+        self.client = client or s3.get_default_client()
+        self.bucket = bucket
+        self.messages = MessageIo("s3://" + self.bucket + "/messages", self.client)
+        self.inputs = InputsIo("s3://" + self.bucket + "/inputs", self.client)
+        self.outputs = OutputsIo("s3://" + self.bucket + "/outputs", self.client)
+        self.control = ControlIo("s3://" + self.bucket + "/control", self.client)
 
 
-def get_input_api(bucket=s3.DEFAULT_S3_BUCKET, client=None):
-    """Return an API for working with inputs using S3 `client`
-    relative to S3 `bucket`.
-    """
-    return InputApi("s3://" + bucket + "/inputs", client)
+def get_io_bundle(bucket=s3.DEFAULT_S3_BUCKET, client=None):
+    return IoBundle(bucket, client)
 
-
-def get_output_api(bucket=s3.DEFAULT_S3_BUCKET, client=None):
-    """Return an API for working with outputs using S3 `client`
-    relative to S3 `bucket`.
-    """
-    return OutputApi("s3://" + bucket + "/outputs", client)
-
-
-def get_control_api(bucket=s3.DEFAULT_S3_BUCKET, client=None):
-    """Return an API for working with outputs using S3 `client`
-    relative to S3 `bucket`.
-    """
-    return ControlApi("s3://" + bucket + "/control", client)
-
-
-MSG = get_message_api()
-IN = get_input_api()
-OUT = get_output_api()
-CTRL = get_control_api()
 
 # ------------------------------------------------------------
 
