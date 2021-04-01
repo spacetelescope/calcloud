6dbf735ae1837d1046fa2751d5fad747b7e0d44c
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2021-04-01 15:55:20 +0000

    black..

--- lambda/EnvCRDS/crds_handler.py
+++ lambda/EnvCRDS/crds_handler.py
@@ -6,6 +6,7 @@ from calcloud import common
 
 import boto3
 
+
 def lambda_handler(event, context):
     print(event)
 
@@ -15,37 +16,32 @@ def lambda_handler(event, context):
     jobdefs = os.environ["JOBDEFINITIONS"].split(",")
 
     for jd in jobdefs:
-        # finding it hard to find the job definitions using the 
+        # finding it hard to find the job definitions using the
         # jbDefinitions list; presumably due to revision number
         # this query returns the active version of the correct
         # job definition for the environment
-        response = client.describe_job_definitions(
-            jobDefinitionName = jd,
-            status="ACTIVE"
-        )
+        response = client.describe_job_definitions(jobDefinitionName=jd, status="ACTIVE")
 
-        for jobdef in response['jobDefinitions']:
+        for jobdef in response["jobDefinitions"]:
             print(jobdef)
             # replace the containerProperties env var for CRDS_CONTEXT
             # with the context in the received message
-            env_vars = jobdef['containerProperties']['environment']
+            env_vars = jobdef["containerProperties"]["environment"]
             for var in env_vars:
-                if var['name'] == 'CRDS_CONTEXT':
-                    var['value'] = f"hst_{crds_context}.pmap"
-            
+                if var["name"] == "CRDS_CONTEXT":
+                    var["value"] = f"hst_{crds_context}.pmap"
+
             # registers a new version of the job definition
             response = client.register_job_definition(
-                jobDefinitionName = jd,
+                jobDefinitionName=jd,
                 type="container",
-                parameters = jobdef['parameters'],
-                containerProperties = jobdef['containerProperties']
+                parameters=jobdef["parameters"],
+                containerProperties=jobdef["containerProperties"],
             )
             print(response)
 
             # deregisters the previous version of the job definition
-            response = client.deregister_job_definition(
-                jobDefinition = jobdef['jobDefinitionArn']
-            )
+            response = client.deregister_job_definition(jobDefinition=jobdef["jobDefinitionArn"])
             print(response)
 
             # it's a bit ridiculous that there's not an API for updating
