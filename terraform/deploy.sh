2b7de301915995fb4d6190f24ec9fa7275fe6911
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2021-04-29 22:12:22 +0000

    version increments

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -3,8 +3,8 @@
 # ADMIN_ARN is set in the ci node env and should not be included in this deploy script
 
 # variables that will likely be changed frequently
-CALCLOUD_VER="0.4.8"
-CALDP_VER="0.2.7"
+CALCLOUD_VER="0.4.9"
+CALDP_VER="0.2.8"
 CAL_BASE_IMAGE="stsci/hst-pipeline:CALDP_20210415_CAL_final"
 
 # this is the tag that the image will have in AWS ECR
