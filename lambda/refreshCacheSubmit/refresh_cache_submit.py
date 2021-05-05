e7f87e329ec78eaa87b7b6de32b50d50d1473418
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2021-05-05 13:39:54 +0000

    black

--- lambda/refreshCacheSubmit/refresh_cache_submit.py
+++ lambda/refreshCacheSubmit/refresh_cache_submit.py
@@ -13,20 +13,17 @@ def lambda_handler(event, context):
 
     # run every time
     rapid_fileshares = {
-        "blackboard" : os.environ["FS_BLACKBOARD"],
-        "crds" : os.environ["FS_CRDS"],
-        "messages" : os.environ["FS_MESSAGES"],
-        "outputs" : os.environ["FS_OUTPUTS"]
+        "blackboard": os.environ["FS_BLACKBOARD"],
+        "crds": os.environ["FS_CRDS"],
+        "messages": os.environ["FS_MESSAGES"],
+        "outputs": os.environ["FS_OUTPUTS"],
     }
 
     # ~once per hour
     # inputs is never written from the cloud
     # the only file someone may want quickly on-prem is the memModel features,
     # but that one is written on-prem so doesn't need a refresh to be visible
-    infrequent_fileshares = {
-        "inputs" : os.environ["FS_INPUTS"],
-        "control" : os.environ["FS_CONTROL"]
-    }
+    infrequent_fileshares = {"inputs": os.environ["FS_INPUTS"], "control": os.environ["FS_CONTROL"]}
 
     for fs_name in rapid_fileshares.keys():
         print(f"{'*'*10} refreshing cache for {fs_name} {'*'*10}")
