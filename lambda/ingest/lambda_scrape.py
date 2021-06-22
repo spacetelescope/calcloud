f35766336f77820cc0a83c8c7a17110d40daf47e
alphasentaurii <rkein@stsci.edu>
2021-06-22 17:31:33 -0400

    removed table status print stmt

--- lambda/ingest/lambda_scrape.py
+++ lambda/ingest/lambda_scrape.py
@@ -292,20 +292,8 @@ def get_ddb_table(table_name):
                 'WriteCapacityUnits': 10
             }
         )
-    print("Table status:", table.table_status)
     return table
 
-
-# class PythonObjectEncoder(JSONEncoder):
-#     def default(self, obj):
-#         if isinstance(obj, (list, dict, str, int, float, bool)):
-#             return JSONEncoder.default(self, obj)
-#         return {'_python_object': pickle.dumps(obj)}
-    
-# def as_python_object(dct):
-#     if '_python_object' in dct:
-#         return pickle.loads(str(dct['_python_object']))
-#     return dct
 
 def create_payload(ipst, features, targets, timestamp):
     data = {
