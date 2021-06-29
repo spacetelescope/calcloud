f3512f93e7d6c405babcdcc25393de589aeb70ba
alphasentaurii <rkein@stsci.edu>
2021-06-29 18:51:08 -0400

    publish lambda function to s3

--- terraform/lambda_ingest.tf
+++ terraform/lambda_ingest.tf
@@ -7,24 +7,23 @@ module "calcloud_lambda_ingest" {
   runtime       = "python3.8"
   create_package       = false
   timeout       = 180
-  cloudwatch_logs_retention_in_days = local.lambda_log_retention_in_days
   s3_existing_package = {
     bucket = "calcloud-modeling${local.environment}"
     key = "lambda/calcloud-ingest.zip"
   }
+  publish = true
+  store_on_s3 = true
+  s3_bucket   = aws_s3_bucket.calcloud_lambda_envs.id
   create_role = false
   attach_cloudwatch_logs_policy = false
-  attach_dead_letter_policy = false
-  attach_network_policy = false
-  attach_tracing_policy = false
-  attach_async_event_policy = false
+  cloudwatch_logs_retention_in_days = local.lambda_log_retention_in_days
 
-  allowed_triggers = {
-    S3TriggerMessage = {
-      principal = "s3.amazonaws.com"
-      source_arn = "arn:aws:s3:${data.aws_region.current.name}:${data.aws_caller_identity.this.account_id}:${aws_s3_bucket.calcloud.id}"
-    }
-  }
+  # allowed_triggers = {
+  #   S3TriggerMessage = {
+  #     principal = "s3.amazonaws.com"
+  #     source_arn = "arn:aws:s3:${data.aws_region.current.name}:${data.aws_caller_identity.this.account_id}:${aws_s3_bucket.calcloud.id}/messages/processed-*.trigger"
+  #   }
+  #}
 
   tags = {
     Name = "calcloud-lambda-ingest${local.environment}"
