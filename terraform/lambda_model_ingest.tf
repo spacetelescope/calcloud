104c8fef348bae258ce23db5a4eed8ddceb3b765
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2021-07-02 13:57:38 +0000

    initial refactor of new model ingest lambda

--- terraform/lambda_model_ingest.tf
+++ terraform/lambda_model_ingest.tf
@@ -1,5 +1,5 @@
-resource "aws_dynamodb_table" "calcloud_hst_db" {
-  name           = "calcloud-hst-db"
+resource "aws_dynamodb_table" "calcloud_model_db" {
+  name           = "calcloud-model${local.environment}"
   billing_mode   = "PROVISIONED"
   read_capacity  = 10
   write_capacity = 10
@@ -11,44 +11,59 @@ resource "aws_dynamodb_table" "calcloud_hst_db" {
   }
 
   tags = {
-    Name        = "calcloud-hst-db"
+    Name        = "calcloud-model${local.environment}"
     Environment = "${local.environment}"
   }
 }
 
-module "calcloud_lambda_ingest" {
+module "lambda_model_ingest" {
   source = "terraform-aws-modules/lambda/aws"
-  function_name = "calcloud-ingest${local.environment}"
+  version = "~> 1.43.0"
+
+  function_name = "calcloud-model-ingest${local.environment}"
+
+  #### NEW ROLE NEEDED HERE
   lambda_role = nonsensitive(data.aws_ssm_parameter.lambda_predict_role.value)
+  ####
+
   description   = "looks for processed-ipppssoot.trigger messages, scrapes and uploads completed job data to DynamoDB"
   handler       = "lambda_scrape.lambda_handler"
   runtime       = "python3.8"
-  create_package       = false
+  publish       = false
   timeout       = 180
-  s3_existing_package = {
-    bucket = "calcloud-modeling${local.environment}"
-    key = "lambda/calcloud-ingest.zip"
-  }
-  publish = true
-  store_on_s3 = true
-  s3_bucket   = aws_s3_bucket.calcloud_lambda_envs.id
-  create_role = false
-  attach_cloudwatch_logs_policy = false
   cloudwatch_logs_retention_in_days = local.lambda_log_retention_in_days
 
+  source_path = [
+    {
+      path = "${path.module}/../lambda/ModelIngest"
+      pip_requirements = true
+    },
+  ]
+
+  store_on_s3 = true
+  s3_bucket   = aws_s3_bucket.calcloud_lambda_envs.id
+
+  # ensures that terraform doesn't try to mess with IAM
+  create_role = false
+  attach_cloudwatch_logs_policy = false
+  attach_dead_letter_policy = false
+  attach_network_policy = false
+  attach_tracing_policy = false
+  attach_async_event_policy = false
+
   environment_variables = {
-    "DDBTABLE": "${aws_dynamodb_table.calcloud_hst_db.name}"
+    "DDBTABLE": "${aws_dynamodb_table.calcloud_model_db.name}"
   }
 
   tags = {
-    Name = "calcloud-lambda-ingest${local.environment}"
+    Name = "calcloud-model-ingest${local.environment}"
   }
 }
 
 resource "aws_lambda_permission" "allow_bucket_ingestLambda" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
-  function_name = module.calcloud_lambda_ingest.this_lambda_function_arn
+  function_name = module.lambda_model_ingest.this_lambda_function_arn
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.calcloud.arn
   source_account = data.aws_caller_identity.this.account_id
