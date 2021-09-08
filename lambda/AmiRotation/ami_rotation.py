be9b092905da5a4cd4d972de5f025f13c225d8ab
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2021-09-08 21:19:30 +0000

    black on ami_rotation.py

--- lambda/AmiRotation/ami_rotation.py
+++ lambda/AmiRotation/ami_rotation.py
@@ -4,15 +4,14 @@ import boto3
 
 ec2 = boto3.client("ec2")
 
+
 def lambda_handler(event, context):
     print(event)
     print(context)
     # print("do ami rotation here")
     ec2.run_instances(
-        LaunchTemplate = {
-            'LaunchTemplateName': os.environ['LAUNCH_TEMPLATE_NAME']
-        },
+        LaunchTemplate={"LaunchTemplateName": os.environ["LAUNCH_TEMPLATE_NAME"]},
         MinCount=1,
         MaxCount=1,
-        SubnetId = os.environ['SUBNET']
+        SubnetId=os.environ["SUBNET"],
     )
