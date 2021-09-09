8a8a22333263f31233f468884b1af404953e75d0
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2021-09-09 14:47:59 +0000

    black a file missing in the code checks

--- ami_rotation/parse_image_json.py
+++ ami_rotation/parse_image_json.py
@@ -4,17 +4,19 @@ from collections import OrderedDict
 from datetime import datetime
 
 response = json.loads(str(sys.argv[1]))
-images = response['Images']
+images = response["Images"]
 
 stsciLinux2Ami = {}
 for image in images:
-  creationDate = image['CreationDate']
-  imageId = image['ImageId']
-  name = image['Name']
-  # Only look at particular AMIs
-  if name.startswith("STSCI-AWS-Linux-2"):
-    stsciLinux2Ami.update({creationDate: imageId})
+    creationDate = image["CreationDate"]
+    imageId = image["ImageId"]
+    name = image["Name"]
+    # Only look at particular AMIs
+    if name.startswith("STSCI-AWS-Linux-2"):
+        stsciLinux2Ami.update({creationDate: imageId})
 # Order the list most recent date first
-orderedAmi = OrderedDict(sorted(stsciLinux2Ami.items(), key = lambda x:datetime.strptime(x[0], '%Y-%m-%dT%H:%M:%S.%f%z'), reverse=True))
+orderedAmi = OrderedDict(
+    sorted(stsciLinux2Ami.items(), key=lambda x: datetime.strptime(x[0], "%Y-%m-%dT%H:%M:%S.%f%z"), reverse=True)
+)
 # Print first element in the ordered dict
 print(list(orderedAmi.values())[0])
