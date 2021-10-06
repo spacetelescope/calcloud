0fb7b0ace7ed2d5762dd9df4b3b60d07fff8e006
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2021-10-06 15:10:49 -0400

    black checking on scripts and re-formatting

--- scripts/dynamo_import.py
+++ scripts/dynamo_import.py
@@ -5,32 +5,30 @@ import argparse
 import csv
 from decimal import Decimal
 
-s3 = boto3.resource('s3')
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+s3 = boto3.resource("s3")
+dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
+
 
 def format_row_item(row):
-    row['timestamp'] = int(row['timestamp'])
-    row['x_files'] = float(row['x_files'])
-    row['x_size'] = float(row['x_size'])
-    row['drizcorr'] = int(row['drizcorr'])
-    row['pctecorr'] = int(row['pctecorr'])
-    row['crsplit'] = int(row['crsplit'])
-    row['subarray'] = int(row['subarray'])
-    row['detector'] = int(row['detector'])
-    row['dtype'] = int(row['dtype'])
-    row['instr'] = int(row['instr'])
-    row['wallclock'] = float(row['wallclock'])
-    row['memory'] = float(row['memory'])
-    row['mem_bin'] = float(row['mem_bin'])
-    row['n_files'] = float(row['n_files'])
-    row['total_mb'] = float(row['total_mb'])
-    row['bin_pred'] = float(row['bin_pred'])
-    row['mem_pred'] = float(row['mem_pred'])
-    row['wall_pred'] = float(row['wall_pred'])
-    return json.loads(
-        json.dumps(row, allow_nan=True),
-        parse_int=Decimal,
-        parse_float=Decimal)
+    row["timestamp"] = int(row["timestamp"])
+    row["x_files"] = float(row["x_files"])
+    row["x_size"] = float(row["x_size"])
+    row["drizcorr"] = int(row["drizcorr"])
+    row["pctecorr"] = int(row["pctecorr"])
+    row["crsplit"] = int(row["crsplit"])
+    row["subarray"] = int(row["subarray"])
+    row["detector"] = int(row["detector"])
+    row["dtype"] = int(row["dtype"])
+    row["instr"] = int(row["instr"])
+    row["wallclock"] = float(row["wallclock"])
+    row["memory"] = float(row["memory"])
+    row["mem_bin"] = float(row["mem_bin"])
+    row["n_files"] = float(row["n_files"])
+    row["total_mb"] = float(row["total_mb"])
+    row["bin_pred"] = float(row["bin_pred"])
+    row["mem_pred"] = float(row["mem_pred"])
+    row["wall_pred"] = float(row["wall_pred"])
+    return json.loads(json.dumps(row, allow_nan=True), parse_int=Decimal, parse_float=Decimal)
 
 
 def write_to_dynamo(rows, table_name):
@@ -42,9 +40,7 @@ def write_to_dynamo(rows, table_name):
         print("Writing batch to DDB...")
         with table.batch_writer() as batch:
             for i in range(len(rows)):
-                batch.put_item(
-                    Item=rows[i]
-                )
+                batch.put_item(Item=rows[i])
     except:
         print("Error executing batch_writer")
 
@@ -65,13 +61,10 @@ def main(key, table_name):
         batch.append(item)
     if batch:
         write_to_dynamo(batch, table_name)
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Uploaded to DynamoDB Table')
-    }
+    return {"statusCode": 200, "body": json.dumps("Uploaded to DynamoDB Table")}
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--table", help="ddb table", type=str)
     parser.add_argument("-k", "--key", help="local csv filepath", type=str)
@@ -79,10 +72,9 @@ if __name__=='__main__':
     if args.table:
         table_name = args.table
     else:
-        table_name = 'calcloud-model-sb'
+        table_name = "calcloud-model-sb"
     if args.key:
         key = args.key
     else:
         key = "latest.csv"
     main(key, table_name)
-    
