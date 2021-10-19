57dda4ef5c16ae8d6bfb2745e3475ca1b6a97ced
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2021-10-19 17:24:56 +0000

    add DDBTABLE env var to rescue lambda

--- terraform/lambda_job_rescue.tf
+++ terraform/lambda_job_rescue.tf
@@ -47,6 +47,7 @@ module "calcloud_lambda_rescueJob" {
   environment_variables = merge(local.common_env_vars, {
       JOBPREDICTLAMBDA = module.lambda_function_container_image.this_lambda_function_arn,
       SUBMIT_TIMEOUT = 14*60,  # leave some room for polling jitter, 14 min vs  15 min above
+      DDBTABLE = "${aws_dynamodb_table.calcloud_model_db.name}"
   })   
 
   tags = {
