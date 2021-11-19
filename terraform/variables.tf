bf4df47b44589e97ae1669e9145938856c0413e6
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2021-11-19 13:08:07 -0500

    default crds context update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -99,8 +99,8 @@ variable crds_context {
   default = {
     "-sb" = "hst_0866.pmap"
     "-dev" = "hst_0866.pmap"
-    "-test" = "hst_0964.pmap"
-    "-ops" = "hst_0964.pmap"
+    "-test" = "hst_0967.pmap"
+    "-ops" = "hst_0967.pmap"
   }
 }
 
