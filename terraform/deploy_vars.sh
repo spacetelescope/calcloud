2a8a6d099addf19e257cd08def359f5884e59941
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2022-01-19 10:05:09 -0500

    new base image CALDP_mvmalpha_CAL_rc2 and rc1 tags

--- terraform/deploy_vars.sh
+++ terraform/deploy_vars.sh
@@ -1,8 +1,8 @@
 #! /bin/bash -xu
 
-export CALCLOUD_VER="v0.4.31"
-export CALDP_VER="v0.2.16"
-export CAL_BASE_IMAGE="stsci/hst-pipeline:CALDP_20211129_CAL_final"
+export CALCLOUD_VER="v0.4.34-rc1"
+export CALDP_VER="v0.2.17-rc1"
+export CAL_BASE_IMAGE="stsci/hst-pipeline:CALDP_mvmalpha_CAL_rc2"
 
 export BASE_IMAGE_TAG=`cut -d ":" -f2- <<< ${CAL_BASE_IMAGE} `
 
