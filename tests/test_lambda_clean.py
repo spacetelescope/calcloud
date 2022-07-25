d650ab66048a2df9fdabbdde796ae5efa35ba1a2
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2022-07-25 16:37:33 -0400

    black on new tests

--- tests/test_lambda_clean.py
+++ tests/test_lambda_clean.py
@@ -10,9 +10,11 @@ from calcloud import io
 
 import clean_handler
 
+
 def assert_empty_messages(comm):
     messages = comm.messages.listl()
-    assert(len(messages)==0)
+    assert len(messages) == 0
+
 
 def setup_diverse_messages(comm):
     """posts messages of most types from io.MESSAGE_TYPES
@@ -40,6 +42,7 @@ def setup_diverse_messages(comm):
 
     return ipppssoots, message_types
 
+
 def setup_ingest_messages(comm):
     """adds a few extra ingested messages to the diverse messages list"""
     ipppssoots, message_types = setup_diverse_messages(comm)
@@ -48,9 +51,9 @@ def setup_ingest_messages(comm):
     # insert the extra ingested messages
     for i in range(3):
         ipst = f"ipppssoo{i+n}"
-        comm.messages.put(f'ingested-{ipst}')
+        comm.messages.put(f"ingested-{ipst}")
         ipppssoots.append(ipst)
-        message_types.append('ingested')
+        message_types.append("ingested")
 
     # read all messages back and assert they match the lists
     mess = comm.messages.listl()
@@ -67,8 +70,8 @@ def get_broadcast_payload(comm):
     # first find the broadcast
     mess = comm.messages.listl()
     found_broadcast = False
-    for i,m in enumerate(mess):
-        if 'broadcast-' in m:
+    for i, m in enumerate(mess):
+        if "broadcast-" in m:
             found_broadcast = True
             break
     # error if the broadcast message wasn't posted
@@ -86,18 +89,19 @@ def test_clean_all_empty(s3_client):
     assert_empty_messages(comm)
 
     # place a clean-all message
-    comm.messages.put('clean-all')
+    comm.messages.put("clean-all")
     mess = comm.messages.listl()
-    assert('clean-all' in mess)
+    assert "clean-all" in mess
 
     # check behavior of clean-all on empty messages prefix
-    all_event = conftest.load_event('clean-event-all.yaml')
+    all_event = conftest.load_event("clean-event-all.yaml")
     with pytest.raises(AssertionError):
         # broadcast assert a non-empty list
         clean_handler.lambda_handler(all_event, {})
     mess = comm.messages.listl()
     # no more messages
     assert_empty_messages(comm)
+
 
 def test_clean_all_ipsts(s3_client):
     """tests clean-all with ipppssoots present
@@ -109,10 +113,10 @@ def test_clean_all_ipsts(s3_client):
     ipppssoots, _ = setup_diverse_messages(comm)
 
     # insert the clean-all
-    comm.messages.put('clean-all')
+    comm.messages.put("clean-all")
 
     # run the lambda
-    all_event = conftest.load_event('clean-event-all.yaml')
+    all_event = conftest.load_event("clean-event-all.yaml")
     clean_handler.lambda_handler(all_event, {})
 
     # make sure clean cleaned up after itself
@@ -123,7 +127,7 @@ def test_clean_all_ipsts(s3_client):
     broadcast_payload = get_broadcast_payload(comm)
     # assert that each ipppssoot has a clean-ipppssoot message in the broadcast payload
     for ipst in ipppssoots:
-        assert f"clean-{ipst}" in broadcast_payload['messages']
+        assert f"clean-{ipst}" in broadcast_payload["messages"]
 
 
 def test_clean_ingested_empty(s3_client):
@@ -133,18 +137,19 @@ def test_clean_ingested_empty(s3_client):
     assert_empty_messages(comm)
 
     # place a clean-ingested message
-    comm.messages.put('clean-ingested')
+    comm.messages.put("clean-ingested")
     mess = comm.messages.listl()
-    assert('clean-ingested' in mess)
+    assert "clean-ingested" in mess
 
     # check behavior of clean-all on empty messages prefix
-    all_event = conftest.load_event('clean-event-ingested.yaml')
+    all_event = conftest.load_event("clean-event-ingested.yaml")
     with pytest.raises(AssertionError):
         # broadcast assert a non-empty list
         clean_handler.lambda_handler(all_event, {})
     mess = comm.messages.listl()
     # no more messages
     assert_empty_messages(comm)
+
 
 def test_clean_ingested_ipsts(s3_client):
     """similar to test_clean_all_ipsts but only the ingested ipppssoot should get into the broadcast payload"""
@@ -153,28 +158,24 @@ def test_clean_ingested_ipsts(s3_client):
     ipppssoots, message_types = setup_ingest_messages(comm)
 
     # count the ingested for validation later
-    ingested_ctr = sum('ingested' in mt for mt in message_types)
+    ingested_ctr = sum("ingested" in mt for mt in message_types)
 
     # insert the clean-ingested
-    comm.messages.put('clean-ingested')
+    comm.messages.put("clean-ingested")
 
     # run the lambda
-    all_event = conftest.load_event('clean-event-ingested.yaml')
+    all_event = conftest.load_event("clean-event-ingested.yaml")
     clean_handler.lambda_handler(all_event, {})
 
     # there should now be a broadcast message with the clean messages as it's payload
     broadcast_payload = get_broadcast_payload(comm)
     # check the number in the payload matches the number of messages we posted
-    payload_ctr = sum('clean' in mess for mess in broadcast_payload['messages'])
-    assert payload_ctr==ingested_ctr
+    payload_ctr = sum("clean" in mess for mess in broadcast_payload["messages"])
+    assert payload_ctr == ingested_ctr
     # assert that only ingested ipppssoots are covered in the broadcast payload
-    for i,ipst in enumerate(ipppssoots):
-        if 'ingested' in message_types[i]:
-            assert f"clean-{ipst}" in broadcast_payload['messages']
+    for i, ipst in enumerate(ipppssoots):
+        if "ingested" in message_types[i]:
+            assert f"clean-{ipst}" in broadcast_payload["messages"]
         else:
             # since we counted the messages and the payload, there can't be spurious messages in the broadcast payload
-            assert f"clean-{ipst}" not in broadcast_payload['messages']
-
-
-
-
+            assert f"clean-{ipst}" not in broadcast_payload["messages"]
