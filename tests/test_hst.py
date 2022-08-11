cba79c8749f27d91875554ad6e9abf8dedb11a11
Jenny Shih <jshih@sizenton2021.stsci.edu>
2022-08-11 11:59:58 -0400

    update test_hst.py

--- tests/test_hst.py
+++ tests/test_hst.py
@@ -1,40 +1,27 @@
 from calcloud import hst
+from . import conftest
 
-IPPPSSOOT_INSTR = {
-    "J": "acs",
-    "U": "wfpc2",
-    "V": "hsp",
-    "W": "wfpc",
-    "X": "foc",
-    "Y": "fos",
-    "Z": "hrs",
-    "E": "eng",
-    "F": "fgs",
-    "I": "wfc3",
-    "N": "nicmos",
-    "O": "stis",
-    "L": "cos",
-}
-
+IPPPSSOOT_INSTR = hst.IPPPSSOOT_INSTR
 INSTRUMENTS = set(IPPPSSOOT_INSTR.values())
 
-def test_get_instrument_output_path():
 
-    output_uri = "s3://tmp/test/output/path"
+def test_get_instrument_output_path():
+    bucket = conftest.BUCKET
+    output_uri = f"s3://{bucket}"
 
     for i in IPPPSSOOT_INSTR.keys():
 
         ipst = f"{i.lower()}pppssoot"
 
         inst = hst.get_instrument(ipst)
-        output_path = hst.get_output_path(output_uri,ipst)
+        output_path = hst.get_output_path(output_uri, ipst)
 
         correct_inst = IPPPSSOOT_INSTR.get(i)
         correct_output_path = f"{output_uri}/{inst}/{ipst}"
 
         assert inst == correct_inst
         assert output_path == correct_output_path
-    
+
     for i in INSTRUMENTS:
         inst = hst.get_instrument(i)
         assert inst == i
