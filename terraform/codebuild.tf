c2a5c7b6b2b24d45146392460491fda293c80ece
Manuel Sanchez <msanchez@stsci.edu>
2022-09-19 02:37:24 +0000

    Removed extra tag for codebuild image

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -12,7 +12,7 @@ resource aws_codebuild_project ami_rotation {
 
     environment {
         compute_type    = "BUILD_GENERAL1_SMALL"
-        image           = "${var.ami_rotation_base_image}-${local.pre_environment}"
+        image           = "${var.ami_rotation_base_image}"
         type            = "LINUX_CONTAINER"
         privileged_mode = true
         image_pull_credentials_type = "SERVICE_ROLE"
