739b03469604343315541852adc6768c2f8f2192
Rob Swaters <rswaters@stsci.edu>
2023-01-30 20:24:29 +0000

    Base image update

--- terraform/deploy_vars.sh
+++ terraform/deploy_vars.sh
@@ -1,8 +1,8 @@
 #! /bin/bash -xu
 
-export CALCLOUD_VER="v0.4.39-rc2"
-export CALDP_VER="v0.2.21-rc2"
-export CAL_BASE_IMAGE="stsci/hst-pipeline:CALDP_drizcosstis_CAL_rc2"
+export CALCLOUD_VER="v0.4.39-rc3"
+export CALDP_VER="v0.2.21-rc3"
+export CAL_BASE_IMAGE="stsci/hst-pipeline:CALDP_drizcosstis_CAL_rc3"
 
 export BASE_IMAGE_TAG=`cut -d ":" -f2- <<< ${CAL_BASE_IMAGE} `
 
