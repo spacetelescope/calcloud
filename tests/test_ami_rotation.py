8726f8b568812287ad3f14d2101bc1150a5561c3
Jenny Shih <jshih@stsci.edu>
2023-06-14 13:04:40 -0400

    add image ID to test ami rotation

--- tests/test_ami_rotation.py
+++ tests/test_ami_rotation.py
@@ -12,8 +12,12 @@ def test_ami_rotation(ec2_resource, ec2_client):
     instances = ec2_resource.instances.filter(Filters=filters)
     assert len(list(instances)) == 0
 
+    # get an image ID
+    image_response = ec2_client.describe_images()
+    image_id = image_response['Images'][0]['ImageId']
+
     # create a launch template
-    ec2_client.create_launch_template(LaunchTemplateName=os.environ["LAUNCH_TEMPLATE_NAME"], LaunchTemplateData={})
+    ec2_client.create_launch_template(LaunchTemplateName=os.environ["LAUNCH_TEMPLATE_NAME"], LaunchTemplateData={"ImageId":image_id})
 
     vpc = ec2_client.create_vpc(CidrBlock="10.0.0.0/16")
 
