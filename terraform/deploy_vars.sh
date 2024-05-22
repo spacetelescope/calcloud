a8b681ea5c4ef52e3d4ec5d1a3de07000e8330b2
Rob Swaters <rswaters@stsci.edu>
2024-05-22 18:35:07 +0000

    Update AMI, deploy_vars.sh

--- terraform/deploy_vars.sh
+++ terraform/deploy_vars.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -xu
-export CALCLOUD_VER="v0.4.43"
-export CALDP_VER="v0.2.25"
+export CALCLOUD_VER="v0.4.44-rc1"
+export CALDP_VER="v0.2.26-rc1"
 export CAL_BASE_IMAGE="stsci/hst-pipeline:CALDP_20240509_CAL_final"
 export BASE_IMAGE_TAG=`cut -d ":" -f2- <<< ${CAL_BASE_IMAGE} `
 
